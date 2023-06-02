300e88eaa8e8319dd4c2e80e75cf6b7262927dab
John Mark Delima <delimajohnmark14@gmail.com>
2023-06-02 17:24:42 +0800

    updated the md

---
+++ network/urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("new_post", views.new_post, name="new_post"),
+    path("profile/<str:username>", views.profile, name="profile"),
+    path("follow/<str:username>", views.follow, name="follow"),
+    path("following", views.following, name="following"),
+    path("post/<int:post_id>", views.post, name="post"),
+    path('search/', views.search_results, name='search_results'),
+    path('edit_profile', views.edit_profile, name='edit_profile'),
+]
