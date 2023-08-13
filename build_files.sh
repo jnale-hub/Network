f9f3e738c88e1340848d368bd58997f15e31f069
John Mark Delima <delimajohnmark14@gmail.com>
2023-08-13 14:58:45 +0800

    Prepare file for deployment

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,15 @@
-pip install -r requirements.txt
+# Build the project
+echo "Building the project..."
+python3 -m pip install -r requirements.txt
 
-# make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+# Make migrations
+echo "Making migrations..."
+python3 manage.py makemigrations
+
+# Migrate the database
+echo "Migrating..."
+python3 manage.py migrate
+
+# Collect static files
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
